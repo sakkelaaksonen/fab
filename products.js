4a0c566f16720b8e5ef99735ce828d2d66ddaa07
Sakari Laaksonen <sakke.laaksonen@gmail.com>
2025-05-20 15:29:29 +0300

    more product details, color contrast fixes, AAA

--- products.js
+++ products.js
@@ -2,31 +2,40 @@ const products = [
     {
         name: "Product 1",
         price: 100,
+        id: 1,
+        description: "This is the first product",
         image: "product1.jpg"
     },
     {
         name: "Product 2",
         price: 200,
+        id: 2,
+        description: "This is the second product",
         image: "product2.jpg"
     },
     {
         name: "Product 3",
         price: 300,
+        id: 3,
+        description: "This is the third product",
         image: "product3.jpg"
     },
     {
         name: "Product 4",
         price: 400,
+        id: 4,
+        description: "This is the fourth product",
         image: "product4.jpg"
     },
     {
         name: "Product 5",
-        price: 500,
+        id: 5,
+        description: "This is the fifth product",
         image: "product5.jpg"
     },
     {
         name: "Product 6",
-        price: 600,
+        id: 6,
         image: "product6.jpg"
     },
     
